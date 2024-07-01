2a17e82a48cd862774f8b54d588192a82da05598
matszpk <matszpk@interia.pl>
2024-07-01 14:29:27 +0200

    Add next testcase for to_aiger.

--- tests/aiger.rs
+++ tests/aiger.rs
@@ -64,4 +64,54 @@ fn test_to_aiger() {
         )
         .as_str()
     );
+    assert_eq!(
+        r##"aag 19 4 0 5 15
+2
+4
+6
+8
+10
+22
+30
+32
+39
+10 2 6
+12 4 6
+14 2 8
+16 4 8
+18 12 14
+20 13 15
+22 19 21
+24 12 14
+26 16 24
+28 17 25
+30 27 29
+32 16 24
+34 22 30
+36 23 31
+38 35 37
+"##,
+        to_aiger_ascii_helper(
+            Circuit::new(
+                4,
+                [
+                    Gate::new_and(0, 2),
+                    Gate::new_and(1, 2),
+                    Gate::new_and(0, 3),
+                    Gate::new_and(1, 3),
+                    // add a1*b0 + a0*b1
+                    Gate::new_xor(5, 6),
+                    Gate::new_and(5, 6),
+                    // add c(a1*b0 + a0*b1) + a1*b1
+                    Gate::new_xor(7, 9),
+                    Gate::new_and(7, 9),
+                    Gate::new_xor(8, 10),
+                ],
+                [(4, false), (8, false), (10, false), (11, false), (12, true)],
+            )
+            .unwrap(),
+            0
+        )
+        .as_str()
+    );
 }
