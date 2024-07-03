e75a033d309dbbcf40c46e8f8d8bef1b090a7330
matszpk <matszpk@interia.pl>
2024-07-03 06:05:21 +0200

    Next testcases for from_aiger.

--- tests/aiger.rs
+++ tests/aiger.rs
@@ -570,6 +570,51 @@ fn test_from_aiger() {
             "16 12 14\n8 13 15\n10 12 15\n4 12 14\n2 13 15\n6 5 3\n"
         )),
     );
+    // inputs as outputs
+    assert_eq!(
+        Ok((
+            Circuit::new(
+                2,
+                [
+                    Gate::new_and(0, 1),
+                    Gate::new_nor(0, 1),
+                    Gate::new_nimpl(0, 1),
+                    Gate::new_xor(0, 1),
+                ],
+                [
+                    (2, false),
+                    (3, false),
+                    (0, true),
+                    (4, false),
+                    (5, false),
+                    (2, true),
+                    (3, true),
+                    (1, false),
+                    (4, true),
+                    (5, true),
+                ]
+            )
+            .unwrap(),
+            vec![
+                (2, AIGEREntry::Var(0, false)),
+                (4, AIGEREntry::Var(1, false)),
+                (6, AIGEREntry::Var(2, false)),
+                (8, AIGEREntry::Var(3, false)),
+                (3, AIGEREntry::Var(0, true)),
+                (10, AIGEREntry::Var(4, false)),
+                (16, AIGEREntry::Var(5, false)),
+                (7, AIGEREntry::Var(2, true)),
+                (9, AIGEREntry::Var(3, true)),
+                (4, AIGEREntry::Var(1, false)),
+                (11, AIGEREntry::Var(4, true)),
+                (17, AIGEREntry::Var(5, true)),
+            ],
+        )),
+        from_aiger_ascii_helper(concat!(
+            "aag 8 2 0 10 6\n2\n4\n6\n8\n3\n10\n16\n7\n9\n4\n11\n17\n",
+            "6 2 4\n8 3 5\n10 2 5\n12 2 4\n14 3 5\n16 13 15\n"
+        )),
+    );
     // latches
     assert_eq!(
         Ok((
@@ -923,6 +968,56 @@ fn test_from_aiger() {
 26 19 25
 28 24 18
 30 27 29
+"##
+        ),
+    );
+    // with acyclic graph - many usages - reordered and gates
+    assert_eq!(
+        Ok((
+            Circuit::new(
+                4,
+                [
+                    Gate::new_xor(0, 1),
+                    Gate::new_and(2, 3),
+                    Gate::new_nimpl(4, 5),
+                    Gate::new_nimpl(0, 4),
+                    Gate::new_nimpl(5, 2),
+                    Gate::new_nor(7, 8),
+                    Gate::new_xor(6, 9),
+                ],
+                [(6, true), (9, false), (10, true)],
+            )
+            .unwrap(),
+            vec![
+                (2, AIGEREntry::Var(0, false)),
+                (4, AIGEREntry::Var(1, false)),
+                (6, AIGEREntry::Var(2, false)),
+                (8, AIGEREntry::Var(3, false)),
+                (19, AIGEREntry::Var(6, true)),
+                (24, AIGEREntry::Var(9, false)),
+                (31, AIGEREntry::Var(10, true))
+            ],
+        )),
+        from_aiger_ascii_helper(
+            r##"aag 15 4 0 3 11
+2
+4
+6
+8
+19
+24
+31
+14 11 13
+12 3 5
+10 2 4
+26 19 25
+16 6 8
+30 27 29
+20 15 2
+18 14 17
+28 24 18
+22 16 7
+24 21 23
 "##
         ),
     );
