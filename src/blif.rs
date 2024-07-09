7da218234b49c7b880a16610947f20c6dcce74c1
matszpk <matszpk@interia.pl>
2024-07-09 16:07:40 +0200

    In BLIF: Routine parse_model returns Model.

--- src/blif.rs
+++ src/blif.rs
@@ -303,8 +303,7 @@ fn parse_model<R: Read>(
     reader: &mut BLIFTokensReader<R>,
     circuit_cache: &mut CircuitCache,
     gate_cache: &mut GateCache,
-    model_map: &mut ModelMap,
-) -> Result<(), BLIFError> {
+) -> Result<Model, BLIFError> {
     // get model name
     let mut model_name = String::new();
     while let Some((line_no, line)) = reader.read_tokens()? {
@@ -508,8 +507,7 @@ fn parse_model<R: Read>(
     }
     // next phase - checking graph of gates and subcircuits - check whether graph have cycles.
     // next phase will be done while resolving graph of models.
-    model_map.insert(model_name, model);
-    Ok(())
+    Ok(model)
 }
 
 #[cfg(test)]
